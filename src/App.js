ea18067c2c20f4698b7f06d31df906a136fd00b5
fighantalenta_ <tatamf.id@gmail.com>
2021-06-19 16:49:53 +0700

    first commit baby

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, lazy, Suspense } from 'react';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { Button , Container } from 'semantic-ui-react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <div class="container-pernyataan" >
+        <div class="container-field" >
+        <div class="container-paragraph" >
+         <h2>Dear , Jhon</h2>
+         <p class="text" >we will develop this app according to our free schedule. <br/> I will inform you if there is an update in this application </p>
+         <p class="text" >if there is no info = we have not updated this application. <br/> because we have many other matters that are more priority for us</p>
+         <br/>
+         <h3>Busy n*gga</h3>
+        </div>
+        <br/>
+        <Button primary >Contact Developer</Button>
+        </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/components/button/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const Button = () => <Button>Click Here</Button>
+
+export default Button
