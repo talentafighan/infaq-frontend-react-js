7c35f16627a896e23e2c28c88e70f139a77526f5
fighantalenta_ <tatamf.id@gmail.com>
2021-06-19 17:22:32 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
         <div class="container-pernyataan" >
         <div class="container-field" >
         <div class="container-paragraph" >
-         <h2>Dear , Jhon</h2>
-         <p class="text" >we will develop this app according to our free schedule. <br/> I will inform you if there is an update in this application </p>
+         <h2>To , Jhon</h2>
+         <p class="text" >we will develop this app n to our free schedule. <br/> I will inform you if there is an update in this application </p>
          <p class="text" >if there is no info = we have not updated this application. <br/> because we have many other matters that are more priority for us</p>
          <br/>
-         <h3>Busy n*gga</h3>
+         <h3>From , Busy n*gga</h3>
         </div>
         <br/>
         <Button primary >Contact Developer</Button>
